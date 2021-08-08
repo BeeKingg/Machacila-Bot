80c62c90fb6d5f3c51093a948ac09fbe86065687
TAKICHAN <87557652+BeeKingg@users.noreply.github.com>
2021-08-08 10:08:43 +0700

    Update carbon.py

--- userbot/modules/carbon.py
+++ userbot/modules/carbon.py
@@ -76,7 +76,7 @@ async def carbon_api(e):
         e.chat_id,
         file,
         caption="Made using [Carbon](https://carbon.now.sh/about/),\
-        \na project by [Dawn Labs](https://dawnlabs.io/)",
+        \na project by [Cece](https://t.me/zoeyvi)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -142,7 +142,7 @@ async def carbon_api(e):
         e.chat_id,
         file,
         caption="Made using [Carbon](https://carbon.now.sh/about/),\
-        \na project by [Dawn Labs](https://dawnlabs.io/)",
+        \na project by [Cece](https://t.me/zoeyvi)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -208,7 +208,7 @@ async def carbon_api(e):
         e.chat_id,
         file,
         caption="Made using [Carbon](https://carbon.now.sh/about/),\
-        \na project by [Dawn Labs](https://dawnlabs.io/)",
+        \na project by [Cece](https://zoeyvi)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -274,7 +274,7 @@ async def carbon_api(e):
         e.chat_id,
         file,
         caption="Made using [Carbon](https://carbon.now.sh/about/),\
-        \na project by [Dawn Labs](https://dawnlabs.io/)",
+        \na project by [Cece](https://t.me/zoeyvi)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -340,7 +340,7 @@ async def carbon_api(e):
         e.chat_id,
         file,
         caption="Made using [Carbon](https://carbon.now.sh/about/),\
-        \na project by [Dawn Labs](https://dawnlabs.io/)",
+        \na project by [Cece](https://t.me/zoeyvi)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
